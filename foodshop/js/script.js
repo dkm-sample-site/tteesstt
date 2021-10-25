e1dff46ea983c2880cec2711748a7b5d193db397
darkmatterjp <ceo@darkmatter.jp>
2021-10-25 14:36:15 +0900

    メニュータグの動作修正

--- foodshop/js/script.js
+++ foodshop/js/script.js
@@ -18,7 +18,7 @@ if (navigator.userAgent.match(/(iPhone|iPad|iPod|Android)/)) {
 //トップへ戻るボタン
 
 $(function() {
-    var topBtn = $('#page-top');    
+    var topBtn = $('#page-top-button');    
     topBtn.hide();
     $(window).scroll(function () {
         if ($(this).scrollTop() > 1000) {
@@ -28,10 +28,11 @@ $(function() {
         }
     });
     if (navigator.userAgent.match(/(iPhone|iPad|iPod|Android)/)) {
-  $(function() {
-	  $("#page-top").hide();
-});
-}
+      $(function() {
+        $("#page-top-button").hide();
+      });
+    }
+
     //スクロールしてトップ
     topBtn.click(function () {
         $('body,html').animate({
@@ -45,7 +46,7 @@ $(function() {
 
 if (navigator.userAgent.match(/(iPhone|iPad|iPod|Android)/)) {
   $(function() {
-	  $("#page-top").hide();
+	  $("#page-top-button").hide();
 });
 }
 
@@ -66,13 +67,49 @@ $(function(){
 //ページ内リンクをヘッダーの高さ分だけずらす
 $(function() {
   var headerHeight = 80;//固定ヘッダーの高さを入れる
-  $('[href^="#"]').click(function(){
+  $('.page_link [href^="#"]').click(function(){
     var href= $(this).attr("href");
     var target = $(href == "#" || href == "" ? 'html' : href);
     var position = target.offset().top-headerHeight; 
     $("html, body").animate({scrollTop:position}, 200, "swing");//200はスクロールの移動スピードです
     return false;
   });
+});
+
+// -------------------
+
+//任意のタブにURLからリンクするための設定
+function GethashID (hashIDName){
+  if(hashIDName){
+    //タブ設定
+    $('.tab li').find('a').each(function() { //タブ内のaタグ全てを取得
+      var idName = $(this).attr('href'); //タブ内のaタグのリンク名（例）#lunchの値を取得 
+      if(idName == hashIDName){ //リンク元の指定されたURLのハッシュタグ（例）http://example.com/#lunch←この#の値とタブ内のリンク名（例）#lunchが同じかをチェック
+        var parentElm = $(this).parent(); //タブ内のaタグの親要素（li）を取得
+        $('.tab li').removeClass("active"); //タブ内のliについているactiveクラスを取り除き
+        $(parentElm).addClass("active"); //リンク元の指定されたURLのハッシュタグとタブ内のリンク名が同じであれば、liにactiveクラスを追加
+        //表示させるエリア設定
+        $(".area").removeClass("is-active"); //もともとついているis-activeクラスを取り除き
+        $(hashIDName).addClass("is-active"); //表示させたいエリアのタブリンク名をクリックしたら、表示エリアにis-activeクラスを追加 
+      }
+    });
+  }
+}
+
+//タブをクリックしたら
+$('.tab a').on('click', function() {
+  var idName = $(this).attr('href'); //タブ内のリンク名を取得  
+  GethashID (idName);//設定したタブの読み込みと
+  return false;//aタグを無効にする
+});
+
+
+// 上記の動きをページが読み込まれたらすぐに動かす
+$(window).on('load', function () {
+    $('.tab li:first-of-type').addClass("active"); //最初のliにactiveクラスを追加
+    $('.area:first-of-type').addClass("is-active"); //最初の.areaにis-activeクラスを追加
+  var hashName = location.hash; //リンク元の指定されたURLのハッシュタグを取得
+  GethashID (hashName);//設定したタブの読み込み
 });
 
  
