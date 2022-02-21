d7b7264cec6901e44dc5130849892a9deace6d09
dkm-suzuki <suzuki@darkmatter.jp>
2022-02-21 19:21:55 +0900

    update standard3a

--- standard3a/js/script.js
+++ standard3a/js/script.js
@@ -1,40 +1,23 @@
 // JavaScript Document
 
-//スマホの時だけ電話番号有効化
-/*
-if (navigator.userAgent.match(/(iPhone|iPad|iPod|Android)/)) {
-  $(function() {
-    $('.tel').each(function() {
-      var str = $(this).html();
-      if ($(this).children().is('img')) {
-        $(this).html($('<a>').attr('href', 'tel:' + $(this).children().attr('alt').replace(/-/g, '')).append(str + '</a>'));
-      } else {
-        $(this).html($('<a>').attr('href', 'tel:' + $(this).text().replace(/-/g, '')).append(str + '</a>'));
-      }
-    });
-  });
-}
-*/
-
-//トップへ戻るボタン
-
+// トップへ戻るボタン
 $(function() {
     var topBtn = $('#page-top');
-//    var topBtn = $('#page-top-button');
     topBtn.hide();
     $(window).scroll(function () {
         if ($(this).scrollTop() > 500) {
-            topBtn.fadeIn();
+            topBtn.fadeIn("slow");
         } else {
-            topBtn.fadeOut();
+            topBtn.fadeOut("slow");
         }
     });
+    // スマホのときだけ、トップへ戻るボタン無効化
     if (navigator.userAgent.match(/(iPhone|iPad|iPod|Android)/)) {
-  $(function() {
-	  $("#page-top").hide();
-});
-}
-    //スクロールしてトップ
+        $(function() {
+	          $("#page-top").hide();
+        });
+    }
+    // スクロールしてトップ
     topBtn.click(function () {
         $('body,html').animate({
             scrollTop: 0
@@ -42,26 +25,3 @@ $(function() {
         return false;
     });
 });
-
-//スマホのときだけ、トップへ戻るボタン無効化
-/*
-if (navigator.userAgent.match(/(iPhone|iPad|iPod|Android)/)) {
-  $(function() {
-	  $("#page-top").hide();
-});
-}
-*/
-
-//スムーススクロール
-/*
-$(function(){
-	$('nav a[href^="#"]').click(function(){
-		var speed = 500;
-		var href= $(this).attr("href");
-		var target = $(href == "#" || href == "" ? 'html' : href);
-		var position = target.offset().top;
-		$("html, body").animate({scrollTop:position}, speed, "swing");
-		return false;
-	});
-});
-*/
